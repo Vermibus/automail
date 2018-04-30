eee8007c57e20cf056a1632fd53a619b3350f8fa
Vermibus <grefkamateusz@gmail.com>
2018-04-30 10:02:01 +0200

    Minor changes in syntax.

--- code/automail.py
+++ code/automail.py
@@ -1,25 +1,24 @@
 import os
-import sys 
-import mail 
+import sys
+import mail
 
 TAG = sys.argv[1]
-#URL = sys.argv[2]
 EMAILS = sys.argv[2:]
 
-# equivalent to: from parsers import TAG.lower(); parser = TAG.lower().Parser() 
+# equivalent to: from parsers import TAG.lower(); parser = TAG.lower().Parser()
 parser = getattr(__import__('parsers', globals(), locals(), [TAG.lower()], 0), TAG.lower()).Parser()
 parser.parse()
 
 mail_variables = parser.getVariables()
 
-# File with raw css&html content of mail 
-mail_content = mail.format(**mail_variables)                   # "/tmp/tmpasdf1234"
+# File with raw css&html content of mail
+mail_content = mail.format(**mail_variables)                   # "/tmp/<random_name>"
 
 receivers = ' '.join( ['-b ' + email for email in EMAILS] )    # "-b e1@mail.com -b e2@gmail.com -b e3@mail.com"
 subject = "-s '[{tag}] {new_price} zł - {item_name}'".format(  # "-s '[tag] 0zł - Any_item_name'"
   tag=TAG,
   new_price = mail_variables['new_price'],
-  item_name = mail_variables['item_name'], 
+  item_name = mail_variables['item_name'],
 )
 
 command = "mutt -e 'set content_type=text/html' {subject} {receivers} < {mail_content}".format(
@@ -27,4 +26,5 @@ command = "mutt -e 'set content_type=text/html' {subject} {receivers} < {mail_co
   receivers = receivers,
   mail_content = mail_content,
 )
+
 os.system(command)
